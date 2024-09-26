0c3846df5015608dd5d53aff632511790d39d3ed
429199-14 <429199-14@A415WS10>
2024-09-26 09:45:13 +0300

    Final

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Output(int num)
+        {
+            int digit = 0;
+            int temp = Math.Abs(num);
+            do
+            {
+                temp /= 10;
+                digit++;
+            } while (temp > 0);
+
+            switch (digit) 
+            {
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(num.ToString() + " ");
+                    Console.ResetColor();
+                    break;
+
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(num.ToString() + " ");
+                    Console.ResetColor();
+                    break;
+
+                case 3:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(num.ToString() + " ");
+                    Console.ResetColor();
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            c = int.Parse(Console.ReadLine());
+
+            int x = Console.WindowWidth / 2;
+            int y = Console.WindowHeight / 2;
+            Console.SetCursorPosition(x, y);
+
+            Output(a);
+            Output(b);
+            Output(c);
+
+            Console.ReadLine();
+        }
+    }
+}
